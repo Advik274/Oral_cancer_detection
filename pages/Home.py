78b64cd65e2fdf2a86663d9066c442d4eb0ac4f0
Arnav Latiyan <66426125+Advik274@users.noreply.github.com>
2026-03-15 23:35:04 +0530

    tesing

--- pages/Home.py
+++ pages/Home.py
@@ -7,7 +7,7 @@ import io
 
 
 def show_home_content():
-    st.write("# Welcome to the OralGuard🦷")
+    st.write("# Welcome to the Oraluard🦷")
 
     def load_lottie_file(filepath: str):
         with open(filepath, 'r', encoding='utf-8') as file:  # Specify UTF-8 encoding
